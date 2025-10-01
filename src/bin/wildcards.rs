c4b4210eac1093d15fe6b7fa4850467afc8c2c38
Morris <itsmorriskiburi@gmail.com>
2025-10-01 20:52:39 +0300

    wildcards

--- src/bin/wildcards.rs
+++ src/bin/wildcards.rs
@@ -1,12 +1,19 @@
 #![allow(unused)]
 
-use actix_web::{get, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{get, guard, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 #[actix_web::main]
 async fn main() {
   HttpServer::new(|| {
     App::new()
-    .service(hello)
+    .service(
+      web::scope("/api")
+      //guard will only allow get requests
+      .guard(guard::Get())
+      .route("/hello/{a:.*}", web::get().to(hello))
+      //this will return a 404 not found error in postman since post request is guarded
+      .route("/world", web::post().to(world))
+    )
   })
   .bind("0.0.0.0:3000")
   .unwrap()
@@ -16,9 +23,13 @@ async fn main() {
 }
 
 //.* mean it will match wildcards(one or more other characters, values or entities in various contexts)
-#[get("/hello/{a:.*}")]
+// #[get("/hello/{a:.*}")]
 async fn hello(req: HttpRequest) -> impl Responder {
   let path = req.match_info().query("a");
   let msg = format!("path: {}", path);
   HttpResponse::Ok().body(msg)
+}
+
+async fn world() -> impl Responder {
+  HttpResponse::Ok().body("world")
 }
