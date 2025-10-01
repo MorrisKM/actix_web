a882a18b4aac8f86aa1e1e3059a2dfbe9a96ff6a
Morris <itsmorriskiburi@gmail.com>
2025-10-01 14:16:05 +0300

    share state

--- src/bin/share_state.rs
+++ src/bin/share_state.rs
@@ -1,3 +1,5 @@
+use std::sync::Mutex;
+
 use actix_web::{get, web::{self, get}, App, HttpResponse, HttpServer, Responder};
 
 #[allow(unused)]
@@ -5,12 +7,16 @@ use actix_web::{get, web::{self, get}, App, HttpResponse, HttpServer, Responder}
 #[actix_web::main]
 async fn main() {
   let person = Person {name: "Morris".to_string(), age: 20};
+  //mutable shared state student
+  let student_data = web::Data::new(Student {name: Mutex::new(String::from("Rewel")), age:Mutex::new(20)});
   HttpServer::new(move|| {
     App::new()
     .app_data(web::Data::new(person.clone()))
+    .app_data(student_data.clone())
     .route("/", get().to(|| async {HttpResponse::Ok().body("get")}))
     .service(hello)
-    .service(world)
+    .service(student)
+    .default_service(web::to(not_found))
   })
   .bind("0.0.0.0:3000")
   .unwrap()
@@ -25,14 +31,31 @@ async fn hello(person: web::Data<Person>) -> impl Responder {
   HttpResponse::Ok().body(msg)
 }
 
-#[get("/world")]
-async fn world(person: web::Data<Person>) -> impl Responder {
-  let msg = format!("Person name is {}, age is {}", person.name, person.age);
+#[get("/student")]
+async fn student(student: web::Data<Student>) -> impl Responder {
+  // how to mutate but use this in another route (handler)
+  // *student.name.lock().unwrap() = String::from("Rewel");
+  // *student.age.lock().unwrap() = 18;
+  let name = student.name.lock().unwrap();
+  let age = student.age.lock().unwrap();
+  let msg = format!("student name is {}, age is {}", name, age);
   HttpResponse::Ok().body(msg)
+  //the HttpResponse can take in more status codes ::InternalServerError for 500
 }
 
 #[derive(Clone)]
 struct Person {
   name: String,
   age: i32
+}
+
+//mutable shared state
+struct Student {
+  name: Mutex<String>,
+  age: Mutex<i32>
+}
+
+
+async fn not_found() -> impl Responder {
+  HttpResponse::NotFound().body("Not Found")
 }
